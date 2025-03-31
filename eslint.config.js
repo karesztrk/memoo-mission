33fbe7c8e197d63ac31a96571feac43ab05bd46f
Károly Török <torok.karoly.krisztian@gmail.com>
2025-03-31 14:07:34 +0200

    A11Y linter setup

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,32 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import eslintPluginAstro from "eslint-plugin-astro";
+import a11yRecommended from "eslint-plugin-jsx-a11y";
+import prettierConfig from "eslint-config-prettier";
+
+const a11y = {
+  plugins: {
+    "jsx-a11y": {
+      rules: a11yRecommended.rules,
+    },
+  },
+  // prettier-ignore
+  /** @type {Record<string, any>} */
+  rules: (a11yRecommended.configs.recommended.rules),
+};
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
+  prettierConfig,
+  a11y,
   {
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
     languageOptions: {
       parserOptions: {
         project: true,
@@ -16,6 +36,7 @@ export default tseslint.config(
     rules: {
       "@typescript-eslint/no-floating-promises": "error",
       "@typescript-eslint/await-thenable": "error",
+      "react/react-in-jsx-scope": "off",
     },
   },
 );
