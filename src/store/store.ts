60354022df6c0000061462e7e6b806903b72f6ab
Károly Török <torok.karoly.krisztian@gmail.com>
2025-04-01 14:31:18 +0200

    Shared state between islands

--- src/store/store.ts
+++ src/store/store.ts
@@ -11,5 +11,38 @@ export const store = configureStore({
   },
 });
 
+export type Store = typeof store;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+// Assign the store to the global object ... so it's available between islands
+if (typeof window !== "undefined") {
+  window.__REDUX_STORE__ = store;
+}
+
+/**
+ * The global store.
+ */
+export const getStore = (): Store => {
+  if (typeof window !== "undefined") {
+    return window.__REDUX_STORE__;
+  }
+  return store;
+};
+
+/**
+ * Subscribe to the global store and call the provided callback whenever the store changes.
+ */
+export const subscribeToStore = <T>(selector: (state: RootState) => T, callback: (value: T) => void) => {
+  let previousState: unknown;
+
+  return getStore().subscribe(() => {
+    const state = getStore().getState();
+    const newSelectedState = selector(state);
+
+    if (newSelectedState !== previousState) {
+      previousState = newSelectedState;
+      callback(newSelectedState);
+    }
+  });
+};
