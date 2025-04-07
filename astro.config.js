0ba212df6c82242eaa2cea081facbf64c7f893e1
Károly Török <torok.karoly.krisztian@gmail.com>
2025-04-07 09:55:03 +0200

    Astro SVG components

--- astro.config.js
+++ astro.config.js
@@ -2,5 +2,8 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 export default defineConfig({
+  experimental: {
+    svg: true,
+  },
   integrations: [react()],
 });
