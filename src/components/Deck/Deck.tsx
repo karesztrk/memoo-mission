70585e423b944b795f0e0b375790e2fbe08a8171
Károly Török <torok.karoly.krisztian@gmail.com>
2025-04-13 13:00:58 +0200

    Removed perfect grid finder

--- src/components/Deck/Deck.tsx
+++ src/components/Deck/Deck.tsx
@@ -1,19 +1,13 @@
-import { useMemo, type FC, type PropsWithChildren } from "react";
+import { type FC, type PropsWithChildren } from "react";
 import "./Deck.css";
-import { greatestDivisor } from "./Deck.util";
 
 interface DeckProps {
   size?: number;
 }
 
 const Deck: FC<PropsWithChildren<DeckProps>> = ({ size = 16, children }) => {
-  const columns = useMemo(() => greatestDivisor(size), [size]);
   return (
-    <div
-      className="deck"
-      style={{ "--columns": columns }}
-      aria-label="Game deck"
-    >
+    <div className="deck" aria-label="Game deck">
       {children}
     </div>
   );
